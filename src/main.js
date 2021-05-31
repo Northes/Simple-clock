56b4c45b00fc1507165bfa8287cf43cdb74822a5
North <45125250+Northes@users.noreply.github.com>
2021-05-31 23:21:40 +0800

    feat: 全局样式

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import dayjs from "dayjs"
 Vue.config.productionTip = false
 Vue.prototype.dayjs = dayjs;//可以全局使用dayjs
 
+import './assets/css/main.css'
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
