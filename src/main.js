0678f815ecfed2b88bbfcd43f5f4b540ad448453
North <45125250+Northes@users.noreply.github.com>
2021-05-31 23:21:16 +0800

    chore: 引入dayjs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import dayjs from "dayjs"
 
 Vue.config.productionTip = false
+Vue.prototype.dayjs = dayjs;//可以全局使用dayjs
 
 new Vue({
   render: h => h(App),
