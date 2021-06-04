56e33f05520a30450d59a5dea974d53801fa1d16
North <45125250+Northes@users.noreply.github.com>
2021-06-05 00:52:47 +0800

    refactor(App.vue): 更新样式

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,13 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
 
-  background-color: #212529;
-  height: 100%;
+  /*background-color: #212529;*/
+  /*height: 100%;*/
 }
 
 
